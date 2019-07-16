19333871549bd8d68465a022537fa320e3b2daaf
Julian Fauricio Valencia Marin <jvalencia@reefstudios.co>
2019-07-15 19:51:16 -0500

    add overflow

--- src/Shared/Redux/Reducers/reminders.reducer.js
+++ src/Shared/Redux/Reducers/reminders.reducer.js
@@ -23,6 +23,33 @@ const initialState = {
     {
       id: 2,
       cityReminder: "cali",
+      selectDay: 10,
+      textReminder: "hola",
+      timeReminder: "13:15",
+      color: "red",
+      timeMilliseconds: 47700
+    },
+    {
+      id: 3,
+      cityReminder: "cali",
+      selectDay: 10,
+      textReminder: "hola",
+      timeReminder: "13:15",
+      color: "red",
+      timeMilliseconds: 47700
+    },
+    {
+      id: 4,
+      cityReminder: "cali",
+      selectDay: 10,
+      textReminder: "hola",
+      timeReminder: "13:15",
+      color: "red",
+      timeMilliseconds: 47700
+    },
+    {
+      id: 5,
+      cityReminder: "cali",
       selectDay: 15,
       textReminder: "hola2",
       timeReminder: "13:20",
